7233ccf6cc8696bec0d14190a81645407544356d
Palash <palash.kanti.kundu.90@gmail.com>
2019-07-01 00:07:17 +0530

    Added lexer first cut

---
+++ java/ganit/src/main/java/in/palash90/ganit/analyzer/Token.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+package in.palash90.ganit.analyzer;
+
+public class Token {
+	private Object value;
+	private TokenType type;
+	private int lineNumber;
+	private int columnNumber;
+
+	public Token(Object tokenValue, TokenType type, int lineNumber, int columnNumber) {
+		this.value = tokenValue;
+		this.type = type;
+		this.lineNumber = lineNumber;
+		this.columnNumber = columnNumber;
+
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public TokenType getType() {
+		return type;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public int getColumnNumber() {
+		return columnNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + this.value + ", " + this.type + ", " + this.lineNumber + ", " + this.columnNumber + "]";
+	}
+
+}
---
+++ java/ganit/src/main/java/in/palash90/ganit/analyzer/TokenType.java
@@ -0,0 +1,8 @@
+package in.palash90.ganit.analyzer;
+
+/**
+ * @author Palash Kanti Kundu
+ */
+public enum TokenType {
+	PLUS, MINUS, MULTIPLY, DIVIDE, MODULO, EXP, SEMI, NUMBER, STRING
+}
